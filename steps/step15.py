c19f6943d76a5366c5a96c25a21d6cc1230d8ab5
Kuroda Hironobu <akaikaze.monokuro@gmail.com>
2019-11-28 12:51:57 +0900

    step 15-16

---
+++ steps/step15.py
@@ -0,0 +1,197 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{} is not supported'.format(type(data)))
+
+        self.data = data
+        self.grad = None
+        self.creator = None
+        self.priority = 0
+
+    def set_creator(self, func):
+        self.creator = func
+        self.priority = func.priority + 1 
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.priority)
+        add_func(self.creator)
+        while funcs:
+            f = funcs.pop()
+            gys = [output.grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+
+                if x.creator is not None:
+                    add_func(x.creator)
+
+    def cleargrad(self):
+        self.grad = None
+
+class Function:
+    def __call__(self, *inputs) -> list:
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs= [Variable(as_array(y)) for y in ys]
+
+        self.priority = max([x.priority for x in inputs])
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError()
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = np.exp(x) * gy
+        return gx
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+    
+    def backward(self, gy):
+        return gy, gy
+
+def square(x):
+    return Square()(x)
+
+def exp(x):
+    return Exp()(x)
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2*eps)
+
+
+import unittest
+
+class SquareTest(unittest.TestCase):
+    def test_forward(self):
+        x = Variable(np.array(2.0))
+        y = square(x)
+        expected = np.array(4.0)
+        self.assertEqual(y.data, expected)
+
+    def test_backward(self):
+        x = Variable(np.array(3.0))
+        y = square(x)
+        y.backward()
+        expected = np.array(6.0)
+        self.assertEqual(x.grad, expected)
+
+    def test_gradient_check(self):
+        x = Variable(np.random.rand(1))
+        y = square(x)
+        y.backward()
+        num_grad = numerical_diff(square, x)
+        flg = np.allclose(x.grad, num_grad)
+        self.assertTrue(flg)
+
+    def test_add_check(self):
+        x1 = Variable(np.array(2))
+        x2 = Variable(np.array(3))
+        # それぞれ変数として与える。
+        y = add(x1, x2)
+        self.assertEqual(y.data, 5.)
+
+    def test_add_square_backward(self):
+        x = Variable(np.array(2.0))
+        y = Variable(np.array(3.0))
+        z = add(square(x), square(y))
+        z.backward()
+        self.assertEqual(z.data, 13.)
+        self.assertEqual(x.grad, 4.)
+        self.assertEqual(y.grad, 6.)
+
+    def test_add_same_variable(self):
+        # 1層
+        x = Variable(np.array(3.0))
+        y = add(x, x)
+        y.backward()
+        self.assertEqual(x.grad, 2.)
+        
+        # 2層
+        x = Variable(np.array(3.0))
+        y = add(add(x, x), x)
+        y.backward()
+        self.assertEqual(x.grad, 3.)
+
+        # 再利用層
+        x.cleargrad()
+        y = add(add(x, x), x)
+        y.backward()
+        self.assertEqual(x.grad, 3.)
+
+        # 分岐有
+        x = Variable(np.array(2.0))
+        a = square(x)
+        y = add(square(a), square(a))
+        y.backward()
+        self.assertEqual(y.data, 32.)
+        self.assertEqual(x.grad, 64.)
+
+    def test_priority_check(self):
+        x = Variable(np.array(2.0))
+        y = Variable(np.array(3.0))
+        z = add(square(x), square(y))
+        z.backward()
+        self.assertEqual(x.priority, 0)
+        self.assertEqual(z.priority, 2)
+        self.assertEqual(z.creator.priority, 1)
+
+    
+if __name__ == '__main__':
+    unittest.main()
