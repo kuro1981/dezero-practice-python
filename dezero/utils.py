a5ced8556dc09125416b73fb8c9d05e946a07d3e
Hironobu Kuroda <hironobu.kuroda@ksk-anl.com>
2019-12-02 18:58:30 +0900

    step 23

---
+++ dezero/utils.py
@@ -0,0 +1,36 @@
+import numpy as np
+import weakref
+import contextlib
+from dezero.core_simple import Function, Variable
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = np.exp(x) * gy
+        return gx
+
+def square(x):
+    return Square()(x)
+
+def exp(x):
+    return Exp()(x)
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2*eps)
+
