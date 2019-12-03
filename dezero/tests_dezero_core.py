745e022ebb29885ec75dfbeb2030a65c647531a8
Hironobu Kuroda <hironobu.kuroda@ksk-anl.com>
2019-12-03 17:39:27 +0900

    step 33

--- dezero/tests_dezero_core.py
+++ dezero/tests_dezero_core.py
@@ -4,8 +4,8 @@ sys.path.append("./")
 import numpy as np
 import unittest
 from dezero import Variable, using_config, no_grad
-from dezero.core_simple import add, mul, div, pow 
-from dezero.utils import numerical_diff, square, exp
+from dezero.core import add, mul, div, pow 
+from dezero.utils import numerical_diff
 from contextlib import contextmanager
 
 class DezeroCoreTest(unittest.TestCase):
@@ -23,72 +23,55 @@ class DezeroCoreTest(unittest.TestCase):
     
     def test_forward(self):
         x = Variable(np.array(2.0))
-        y = square(x)
+        y = x ** 2
         expected = np.array(4.0)
         self.assertEqual(y.data, expected)
 
     def test_backward(self):
         x = Variable(np.array(3.0))
-        y = square(x)
+        y = x ** 2
         y.backward()
         expected = np.array(6.0)
-        self.assertEqual(x.grad, expected)
-
-    def test_gradient_check(self):
-        x = Variable(np.random.rand(1))
-        y = square(x)
-        y.backward()
-        num_grad = numerical_diff(square, x)
-        flg = np.allclose(x.grad, num_grad)
-        self.assertTrue(flg)
+        self.assertEqual(x.grad.data, expected)
 
     def test_add_check(self):
         x0 = Variable(np.array(2))
         x1 = Variable(np.array(3))
         # それぞれ変数として与える。
         y = add(x0, x1)
-        self.assertEqual(y.data, 5.)
-
-    def test_add_square_backward(self):
-        x = Variable(np.array(2.0))
-        y = Variable(np.array(3.0))
-        z = add(square(x), square(y))
-        z.backward()
-        self.assertEqual(z.data, 13.)
-        self.assertEqual(x.grad, 4.)
-        self.assertEqual(y.grad, 6.)
+        self.assertEqual(y.data, np.array(5))
 
     def test_add_same_variable(self):
         # 1層
         x = Variable(np.array(3.0))
         y = add(x, x)
         y.backward()
-        self.assertEqual(x.grad, 2.)
+        self.assertEqual(x.grad.data, 2.)
         
         # 2層
         x = Variable(np.array(3.0))
         y = add(add(x, x), x)
         y.backward()
-        self.assertEqual(x.grad, 3.)
+        self.assertEqual(x.grad.data, 3.)
 
         # 再利用層
         x.cleargrad()
         y = add(add(x, x), x)
         y.backward()
-        self.assertEqual(x.grad, 3.)
+        self.assertEqual(x.grad.data, 3.)
 
         # 分岐有
         x = Variable(np.array(2.0))
-        a = square(x)
-        y = add(square(a), square(a))
+        a = x ** 2
+        y = add(a ** 2, a ** 2)
         y.backward()
         self.assertEqual(y.data, 32.)
-        self.assertEqual(x.grad, 64.)
+        self.assertEqual(x.grad.data, 64.)
 
     def test_priority_check(self):
         x = Variable(np.array(2.0))
         y = Variable(np.array(3.0))
-        z = add(square(x), square(y))
+        z = add(x ** 2, y ** 2)
         z.backward()
         self.assertEqual(x.priority, 0)
         self.assertEqual(z.priority, 2)
@@ -105,27 +88,27 @@ class DezeroCoreTest(unittest.TestCase):
         
         self.assertIsNone(y.grad)
         self.assertIsNone(t.grad)
-        self.assertIsNotNone(x0.grad)
-        self.assertIsNotNone(x1.grad)
+        self.assertIsNotNone(x0.grad.data)
+        self.assertIsNotNone(x1.grad.data)
        
         # retain_grad True
         x0.cleargrad()
         x1.cleargrad()
         y.backward(retain_grad = True)
-        self.assertIsNotNone(y.grad)
-        self.assertIsNotNone(t.grad)
+        self.assertIsNotNone(y.grad.data)
+        self.assertIsNotNone(t.grad.data)
         
     def test_config_enable_backprop_check(self):
         # backprop True
         x = Variable(np.ones((100,100,100)))
-        y = square(square(square(x)))
+        y = x ** 3
         with self.assertNotRaises(Exception):
             y.backward()    
 
         # backprop False
         with no_grad():
             x.cleargrad()
-            y = square(square(square(x)))
+            y = x ** 3
             with self.assertRaises(Exception):
                 y.backward()    
     
@@ -153,16 +136,16 @@ class DezeroCoreTest(unittest.TestCase):
         y = add(mul(a, b), c)
         y.backward()
         self.assertEqual(y.data, np.array(8.))
-        self.assertEqual(a.grad, 2.)
-        self.assertEqual(b.grad, 3.)
+        self.assertEqual(a.grad.data, 2.)
+        self.assertEqual(b.grad.data, 3.)
 
         y.cleargrad()
         a.cleargrad()
         y = a * np.array(2.0) + 1.0
         y.backward()
         self.assertEqual(y.data, np.array(7.))
-        self.assertEqual(a.grad, 2.)
-        self.assertEqual(b.grad, 3.)
+        self.assertEqual(a.grad.data, 2.)
+        self.assertEqual(b.grad.data, 3.)
         
     def test_underscore_add_mul(self):
         a = Variable(np.array(3.0))
@@ -173,8 +156,8 @@ class DezeroCoreTest(unittest.TestCase):
         y.backward()
         
         self.assertEqual(y.data, np.array(6.))
-        self.assertEqual(a.grad, 2.)
-        self.assertEqual(b.grad, 3.)
+        self.assertEqual(a.grad.data, 2.)
+        self.assertEqual(b.grad.data, 3.)
 
         y.cleargrad()
         y = (a * b) + c
@@ -186,7 +169,7 @@ class DezeroCoreTest(unittest.TestCase):
         y = -x
         y.backward()
         self.assertEqual(y.data, np.array(-3.0))
-        self.assertEqual(x.grad, -1.)
+        self.assertEqual(x.grad.data, -1.)
 
     def test_sub(self):
         x = Variable(np.array(3.0))
@@ -207,8 +190,21 @@ class DezeroCoreTest(unittest.TestCase):
         y = x ** 5
         y.backward()
         self.assertEqual(y.data, np.array(3.0 ** 5))
-        self.assertEqual(x.grad, 5.0 * 3 ** (4))
-        
+        self.assertEqual(x.grad.data, 5.0 * 3 ** (4))
+    
+    def test_second_diff(self):
+        def f(x):
+            y = x ** 4 - 2 * x ** 2
+            return y
+        x = Variable(np.array(2.0))
+        y = f(x)
+        y.backward(create_graph=True)
+        self.assertEqual(x.grad.data, 24.)
+
+        gx = x.grad
+        x.cleargrad()
+        gx.backward()
+        self.assertEqual(x.grad.data, 44.)
         
 if __name__ == '__main__':
     unittest.main()
