06a84112fc2a960f1fba7a90be320a11bcf6ed3d
Hironobu Kuroda <hironobu.kuroda@ksk-anl.com>
2019-12-03 18:25:18 +0900

    step 36

---
+++ steps/step36.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append("./")
+
+import numpy as np
+from dezero import Variable
+import dezero.functions as F
+
+x = Variable(np.array(2.0))
+y = x ** 2
+y.backward(create_graph=True)
+gx = x.grad
+x.cleargrad()
+
+z = gx ** 3 + y
+z.backward()
+print(x.grad)
