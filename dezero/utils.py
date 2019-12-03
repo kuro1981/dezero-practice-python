837360966c38173fe38932fe01a87ea7146b9f58
Hironobu Kuroda <hironobu.kuroda@ksk-anl.com>
2019-12-03 13:46:04 +0900

    step 26

--- dezero/utils.py
+++ dezero/utils.py
@@ -34,3 +34,44 @@ def numerical_diff(f, x, eps=1e-4):
     y1 = f(x1)
     return (y1.data - y0.data) / (2*eps)
 
+def _dot_var(v):
+    dot_var = '{} [label="{}", color=orange, style=filled]\n'
+    name = ''
+    if hasattr(v, 'name') and v.name is not None:
+        name = v.name
+    return dot_var.format(id(v), name)
+
+def _dot_func(f):
+    dot_func = '{} [label="{}", color=lightblue, style=filled, shape=box]\n'
+    txt = dot_func.format(id(f), f.__class__.__name__)
+
+    dot_edge = '{} -> {}\n'
+    for x in f.inputs:
+        txt += dot_edge.format(id(x), id(f))
+    for y in f.outputs:
+        txt += dot_edge.format(id(f), id(y())) # y is weakref
+    return txt
+
+def get_dot_graph(y):
+    funcs = []
+    seen_set = set()
+
+    def add_func(f):
+        if f not in seen_set:
+            funcs.append(f)
+            seen_set.add(f)
+
+    add_func(y.creator)
+    txt = _dot_var(y)
+
+    while funcs:
+        func = funcs.pop()
+        txt += _dot_func(func)
+        for x in func.inputs:
+            txt += _dot_var(x)
+
+            if x.creator is not None:
+                add_func(x.creator)
+
+    return 'digraph g {\n' + txt + '}'
+
---
+++ steps/step26.py
@@ -0,0 +1,57 @@
+import sys
+sys.path.append("./")
+
+import numpy as np
+from dezero import Variable
+from dezero.utils import get_dot_graph
+
+def sphere(x, y):
+    y = x ** 2 + y ** 2
+    return y
+
+def matyas(x, y):
+    z = 0.26 * (x**2 + y**2) - 0.48 * x * y
+    return z
+
+def goldstein(x, y):
+    y = (1 +(x+y+1)**2 * (19 - 14*x + 3*x**2 - 14*y + 6*x*y + 3*y**2)) * \
+        (30 + (2*x - 3*y)**2 * (18 - 32*x + 12*x**2 + 48*y -36*x*y + 27*y**2))
+    return y
+
+import unittest
+
+class OptimizationTest(unittest.TestCase):
+    def test_sphere(self):
+        x = Variable(np.array(1.0))
+        y = Variable(np.array(2.0))
+        z = sphere(x, y)
+        z.backward()
+
+        self.assertEqual(x.grad, 2.)
+        self.assertEqual(y.grad, 4.)
+
+    def test_matyas(self):
+        x = Variable(np.array(1.0))
+        y = Variable(np.array(1.0))
+        z = matyas(x, y)
+        z.backward()
+        
+    def test_goldstein(self):
+        x0 = Variable(np.array(1.0))
+        x1 = Variable(np.array(1.0))
+        y= goldstein(x0, x1)
+        y.backward()
+
+        self.assertEqual(x0.grad, -5376.)
+        self.assertEqual(x1.grad, 8064.0)
+        
+        x0.name = 'x0'
+        x1.name = 'x1'
+        y.name = 'y'
+        with open('graph.dot',"w") as f:
+            f.write(get_dot_graph(y))
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
