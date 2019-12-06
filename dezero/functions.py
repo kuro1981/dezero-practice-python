108336ac3df49eb7a1cd3e37b2ad2bd084416170
Hironobu Kuroda <hironobu.kuroda@ksk-anl.com>
2019-12-06 10:37:57 +0900

    step 42

--- dezero/functions.py
+++ dezero/functions.py
@@ -89,6 +89,23 @@ class MatMul(Function):
 def matmul(x, W):
     return MatMul()(x, W)
 
+class MeanSquaredError(Function):
+    def forward(self, x0, x1):
+        diff = x0 - x1
+        y = (diff ** 2).sum() / diff.size()
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs
+        diff = x0 - x1
+        gy = broadcast_to(gy, diff.shape)
+        gx0 = gy * diff * (2. / diff.size)
+        gx1 = -gx0
+        return gx0, gx1
+
+def mean_squared_error(x0, x1):
+    return MeanSquaredError()(x0, x1)
+
 def sin(x):
     return Sin()(x)
 
